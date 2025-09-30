0251b303ac89e6af5ee07a5559045253a1f45e23
Shadow Knight <shadowknight0316@gmail.com>
2025-09-30 18:59:21 +0000

    the bottom bar is a must to be shown and you have to remove it from the

--- src/app/lab/layout.tsx
+++ src/app/lab/layout.tsx
@@ -27,7 +27,7 @@ function LayoutContent({ children }: { children: React.ReactNode }) {
   }, [heldItem, heldEquipment]);
 
   return (
-    <div className={cn("flex flex-col h-screen overflow-hidden")}>
+    <div className={cn("flex flex-col h-screen")}>
       <main className="flex-1 overflow-y-auto">
         {children}
       </main>
